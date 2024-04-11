82e0dceeaadaa5bfa93d352161c3b13ab71d3a06
Luc Lispe <lunareclipsewasshort@gmail.com>
2024-04-11 14:53:17 +0300

    initial commit

---
+++ projects/modal-auth-html/bundle.ts
@@ -0,0 +1,1 @@
+export { WalletConnectModalAuth } from './src/client'
---
+++ projects/modal-auth-html/index.ts
@@ -0,0 +1,5 @@
+export { WalletConnectModalAuth } from './src/client'
+export type {
+  WalletConnectModalAuthOptions,
+  WalletConnectModalAuthSignInArguments
+} from './src/client'
